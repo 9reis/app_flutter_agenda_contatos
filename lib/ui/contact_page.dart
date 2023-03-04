3ce251c778ffd675c3f0566e4f5f5080a912e52e
Lucas Reis <lucasreis_9@hotmail.com>
2023-03-04 20:53:20 -0300

    Layout da tela de contato

--- lib/ui/contact_page.dart
+++ lib/ui/contact_page.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:app_flutter_agenda_contatos/helpers/contact_helper.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
@@ -6,7 +8,7 @@ import 'package:flutter/src/widgets/placeholder.dart';
 class ContactPage extends StatefulWidget {
   final Contact? contact;
 
-  const ContactPage({Key? key, this.contact}) : super(key: key);
+  ContactPage({Key? key, this.contact}) : super(key: key);
 
   @override
   State<ContactPage> createState() => _ContactPageState();
@@ -15,6 +17,12 @@ class ContactPage extends StatefulWidget {
 class _ContactPageState extends State<ContactPage> {
   Contact? _editedContact;
 
+  bool _userEditted = false;
+
+  final _nameController = TextEditingController();
+  final _emailController = TextEditingController();
+  final _phoneController = TextEditingController();
+
   @override
   void initState() {
     super.initState();
@@ -22,6 +30,10 @@ class _ContactPageState extends State<ContactPage> {
       _editedContact = Contact();
     } else {
       _editedContact = Contact.fromMap(widget.contact!.toMap());
+
+      _nameController.text = _editedContact!.name!;
+      _emailController.text = _editedContact!.email!;
+      _phoneController.text = _editedContact!.phone!;
     }
   }
 
@@ -30,13 +42,62 @@ class _ContactPageState extends State<ContactPage> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.red,
-        title: Text(_editedContact?.name ?? "Novo Contato"),
+        title: Text(_editedContact!.name ?? "Novo Contato"),
         centerTitle: true,
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
         child: Icon(Icons.save),
         backgroundColor: Colors.red,
+      ),
+      body: SingleChildScrollView(
+        padding: EdgeInsets.all(10),
+        child: Column(
+          children: [
+            GestureDetector(
+              child: Container(
+                width: 140,
+                height: 140,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  image: DecorationImage(
+                    image: _editedContact!.img != null
+                        ? FileImage(File(_editedContact!.img!))
+                        : AssetImage("images/perfil.png") as ImageProvider,
+                  ),
+                ),
+              ),
+            ),
+            TextField(
+              controller: _nameController,
+              decoration: const InputDecoration(labelText: "Nome"),
+              onChanged: (text) {
+                _userEditted = true;
+                setState(() {
+                  _editedContact!.name = text;
+                });
+              },
+            ),
+            TextField(
+              controller: _emailController,
+              decoration: const InputDecoration(labelText: "Email"),
+              onChanged: (text) {
+                _userEditted = true;
+                _editedContact!.email = text;
+              },
+              keyboardType: TextInputType.emailAddress,
+            ),
+            TextField(
+              controller: _phoneController,
+              decoration: const InputDecoration(labelText: "Phone"),
+              onChanged: (text) {
+                _userEditted = true;
+                _editedContact!.phone = text;
+              },
+              keyboardType: TextInputType.phone,
+            ),
+          ],
+        ),
       ),
     );
   }
