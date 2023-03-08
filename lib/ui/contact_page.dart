9b368554d806b6c93152b8ffc81e9423b867fe0c
Lucas Reis <lucasreis_9@hotmail.com>
2023-03-07 22:07:06 -0300

    Exibindo um AlertDialog

--- lib/ui/contact_page.dart
+++ lib/ui/contact_page.dart
@@ -42,77 +42,113 @@ class _ContactPageState extends State<ContactPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.red,
-        title: Text(_editedContact!.name ?? "Novo Contato"),
-        centerTitle: true,
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          if (_editedContact!.name != null) {
-            // Remove a tela atual e manda para tela anterior o contato editado
-            Navigator.pop(context, _editedContact);
-          } else if (_editedContact!.phone == null) {
-            FocusScope.of(context).requestFocus(_phoneFocus);
-          } else {
-            FocusScope.of(context).requestFocus(_nameFocus);
-          }
-        },
-        child: Icon(Icons.save),
-        backgroundColor: Colors.red,
-      ),
-      body: SingleChildScrollView(
-        padding: EdgeInsets.all(10),
-        child: Column(
-          children: [
-            GestureDetector(
-              child: Container(
-                width: 140,
-                height: 140,
-                decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  image: DecorationImage(
-                    image: _editedContact!.img != null
-                        ? FileImage(File(_editedContact!.img!))
-                        : AssetImage("images/perfil.png") as ImageProvider,
+    return WillPopScope(
+      onWillPop: _requestPop,
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.red,
+          title: Text(_editedContact!.name ?? "Novo Contato"),
+          centerTitle: true,
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            if (_editedContact!.name != null) {
+              // Remove a tela atual e manda para tela anterior o contato editado
+              Navigator.pop(context, _editedContact);
+            } else if (_editedContact!.phone == null) {
+              FocusScope.of(context).requestFocus(_phoneFocus);
+            } else {
+              FocusScope.of(context).requestFocus(_nameFocus);
+            }
+          },
+          child: Icon(Icons.save),
+          backgroundColor: Colors.red,
+        ),
+        body: SingleChildScrollView(
+          padding: EdgeInsets.all(10),
+          child: Column(
+            children: [
+              GestureDetector(
+                child: Container(
+                  width: 140,
+                  height: 140,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    image: DecorationImage(
+                      image: _editedContact!.img != null
+                          ? FileImage(File(_editedContact!.img!))
+                          : AssetImage("images/perfil.png") as ImageProvider,
+                    ),
                   ),
                 ),
               ),
-            ),
-            TextField(
-              controller: _nameController,
-              focusNode: _nameFocus,
-              decoration: const InputDecoration(labelText: "Nome"),
-              onChanged: (text) {
-                _userEditted = true;
-                setState(() {
-                  _editedContact!.name = text;
-                });
-              },
-            ),
-            TextField(
-              controller: _emailController,
-              decoration: const InputDecoration(labelText: "Email"),
-              onChanged: (text) {
-                _userEditted = true;
-                _editedContact!.email = text;
-              },
-              keyboardType: TextInputType.emailAddress,
-            ),
-            TextField(
-              controller: _phoneController,
-              focusNode: _phoneFocus,
-              decoration: const InputDecoration(labelText: "Phone"),
-              onChanged: (text) {
-                _userEditted = true;
-                _editedContact!.phone = text;
-              },
-              keyboardType: TextInputType.phone,
-            ),
-          ],
+              TextField(
+                controller: _nameController,
+                focusNode: _nameFocus,
+                decoration: const InputDecoration(labelText: "Nome"),
+                onChanged: (text) {
+                  _userEditted = true;
+                  setState(() {
+                    _editedContact!.name = text;
+                  });
+                },
+              ),
+              TextField(
+                controller: _emailController,
+                decoration: const InputDecoration(labelText: "Email"),
+                onChanged: (text) {
+                  _userEditted = true;
+                  _editedContact!.email = text;
+                },
+                keyboardType: TextInputType.emailAddress,
+              ),
+              TextField(
+                controller: _phoneController,
+                focusNode: _phoneFocus,
+                decoration: const InputDecoration(labelText: "Phone"),
+                onChanged: (text) {
+                  _userEditted = true;
+                  _editedContact!.phone = text;
+                },
+                keyboardType: TextInputType.phone,
+              ),
+            ],
+          ),
         ),
       ),
     );
+  }
+
+  Future<bool> _requestPop() {
+    if (_userEditted) {
+      showDialog(
+          context: context,
+          builder: (context) {
+            return AlertDialog(
+              title: Text('Descartar Alterações?'),
+              content: Text('Se sair as alterações serão perdidas'),
+              actions: [
+                TextButton(
+                  child: Text('Cancelar'),
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                ),
+                TextButton(
+                  child: Text('Sim'),
+                  onPressed: () {
+                    Navigator.pop(context);
+                    Navigator.pop(context);
+                  },
+                )
+              ],
+            );
+          });
+      // Não sai da tela caso tenha feito modificações
+      return Future.value(false);
+    } else {
+      //Permite sair da tela
+      return Future.value(true);
+    }
   }
 }
